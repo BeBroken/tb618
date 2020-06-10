b8021b835fa16795ff6421a02f7f7ab8c58ae8de
Owen Chang <small_2009@yeah.net>
2020-06-10 10:22:51 +0800

    领喵币逻辑更新，支持多个位置进入618界面，新增多个窗口定位方法

--- tb618.js
+++ tb618.js
@@ -1,12 +1,12 @@
 /**
  * @file   tb618.js
- * @brief  淘宝618活动自动领瞄币脚本
+ * @brief  淘宝618活动自动领喵币脚本
  * @author ZehuanZhang (https://github.com/ZehuanZhang)
  * @detail 本脚本任何人可自由使用，不包含任何收费、协助点击、分享等内容，
  *         若需移植、修改或分享，请保留该文件头，尊重劳动成果！
  */
 // 检查无障碍模式是否启用
-if("undefined" == typeof GlobalDisableWaitFor) var GlobalDisableWaitFor = false;
+if ("undefined" == typeof GlobalDisableWaitFor) var GlobalDisableWaitFor = false;
 if (!GlobalDisableWaitFor) {
     ShowMessage("检测无障碍模式是否开启");
     if (app.versionCode >= 400) {
@@ -16,7 +16,7 @@ if (!GlobalDisableWaitFor) {
         ShowMessage("版本号低于400，请手动检查无障碍模式")
     }
 }
-else{
+else {
     ShowMessage("无障碍模式检测已关闭");
 }
 // 获取设备屏幕信息
@@ -24,7 +24,7 @@ var height = device.height;
 var width = device.width;
 
 ShowMessage(
-    "欢迎来到领瞄币脚本\n" +
+    "欢迎来到领喵币脚本\n" +
     "Github项目: tb618\n" +
     "设备宽: " + width + "\n" +
     "设备高: " + height + "\n" +
@@ -38,62 +38,93 @@ sleep(3000);
 
 // 进入活动界面
 CheckAndGoActivity(true);
-
-// 执行领瞄币操作
-DoActions();
+// 签到
+DoClickAction("签到");
+// 执行领喵币操作
+while(DoActions());
 ShowMessage("结束")
 ShowMessage(
     "Github项目: tb618\n" +
     "好用的话请点个Star哦!");
 
 function DoActions() {
-    DoClickAction("签到");
-    DoClickAction("去兑换");
-    DoVisitAction("去浏览");
-    DoVisitAction("去完成");
-    DoVisitAction("去进店");
-    DoVisitAction("去逛逛");
-    DoVisitAction("去围观");
-    DoLookAction("去观看");
-    DoFarmAction();
+    var ActionsList = className("android.widget.ListView").depth(16).findOnce();
+    var retVal = false;
+    if (ActionsList) {
+        var NBChild = ActionsList.childCount();
+        var incFlag = true;
+        for (var i = 0; incFlag && i < NBChild; ++i) {
+            incFlag = false;
+            var ActionContext = ActionsList.child(i);
+            var ActBtn = ActionContext.child(1);
+            var ActContent = ActionContext.child(0);
+            switch (ActBtn.text()) {
+                case "去浏览":
+                case "去逛逛":
+                case "去完成":
+                case "去围观":
+                case "去进店":
+                    {
+                        var re = new RegExp("(浏览|逛一逛).*喵币");
+                        if (re.test(ActContent.text())) {
+                            DoVisitAction(ActBtn);
+                        }
+                        else {
+                            var astr = ActContent.text();
+                            astr = astr.substring(0, astr.indexOf("("));
+                            ShowMessage("不支持\"" + astr + "\"");
+                            incFlag = true;
+                        }
+                    } break;
+                case "签到":
+                case "去兑换":
+                    DoClickAction(ActBtn.text()); break;
+                case "去观看":
+                    DoLookAction("去观看"); break;
+                case "去收菜":
+                    DoFarmAction(); break;
+                default:
+                    incFlag = true;
+                    break;
+            }
+        }
+        if(!incFlag) retVal = true;
+    }
+    return retVal;
 }
 
 function WaitActionFinished(Timeout) {
-    if(!Timeout) Timeout = 15000;
+    if (!Timeout) Timeout = 15000;
 
     var Timer = 0
     // 这个等待最多15s
     while (
         Timer <= 15000 &&
-        !descMatches(" ?任务已?完成").exists() &&
-        !textMatches(" ?任务已?完成").exists()
+        !descMatches("(.*)?任务已?完成(.*)?").exists() &&
+        !textMatches("(.*)?任务已?完成(.*)?").exists()
     ) {
         sleep(500);
         Timer += 500;
     }
 }
 
-function DoVisitAction(actionName) {
-    if (!text(actionName).exists()) return;
+function DoVisitAction(ActBtn) {
+    if(!ActBtn) return;
 
+    var actionName = ActBtn.text();
     ShowMessage("准备" + actionName);
-    while (text(actionName).exists()) {
-        ShowMessage("存在" + actionName);
-        text(actionName).findOnce().click();
-        sleep(1500);
-        swipe(width / 2, height - 400, width / 2, 0, 1000);
-        sleep(5000);
-        swipe(width / 2, height - 400, width / 2, 0, 1000);
-        sleep(5000);
-        swipe(width / 2, height - 400, width / 2, 0, 1000);
-        // 鉴于前面操作需要一部分时间，这里减少一些
-        WaitActionFinished(10000);
-        sleep(500);
-        back();
-        sleep(1000);
-        // 防止淘宝骚操作，若返回主界面，尝试重新进入活动界面
-        CheckAndGoActivity();
-    }
+    text(actionName).findOnce().click();
+    sleep(1500);
+    swipe(width / 2, height - 400, width / 2, 0, 1000);
+    sleep(5000);
+    swipe(width / 2, height - 400, width / 2, 0, 1000);
+    sleep(5000);
+    swipe(width / 2, height - 400, width / 2, 0, 1000);
+    // 鉴于前面操作需要一部分时间，这里减少一些
+    WaitActionFinished(8000);
+    back();
+    // 防止淘宝骚操作，若返回主界面，尝试重新进入活动界面
+    CheckAndGoActivity();
     ShowMessage("完成" + actionName);
 }
 
@@ -119,7 +150,7 @@ function ShowMessage(msg) {
 
 function DoLookAction(actionName) {
     if (!text(actionName).exists()) return;
-    
+
     ShowMessage("准备" + actionName)
     while (text(actionName).exists()) {
         ShowMessage("存在" + actionName);
@@ -132,19 +163,25 @@ function DoLookAction(actionName) {
     ShowMessage("完成" + actionName);
 }
 
-function DoFarmAction(){
+function DoFarmAction() {
     actionName = "去收菜";
     var Btn = text(actionName).findOnce();
-    if (Btn) 
+    if (Btn)
         Btn.click();
     else
         return;
     ShowMessage("准备" + actionName);
+    var OneMoreBack = false;
+    var JumpBtn = className("android.view.View").clickable(true).depth(15).indexInParent(0).findOnce();
+    if (JumpBtn) {
+        JumpBtn.click();
+        OneMoreBack = true;
+    }
     sleep(4000);
     var GatherBtn = className("android.view.View").text("立即去收").findOnce();
-    if(GatherBtn){
+    if (GatherBtn) {
         var GatherBtnBounds = GatherBtn.bounds();
-        click(GatherBtn.centerX(), GatherBtn.centerY());
+        click(GatherBtnBounds.centerX(), GatherBtnBounds.centerY());
         sleep(2000);
     }
     var GameRegion = className("android.view.View").depth(8).indexInParent(1).findOnce();
@@ -162,9 +199,11 @@ function DoFarmAction(){
         sleep(1500);
         back();
     }
-    else 
+    else{
         ShowMessage("无法定位界面");
-
+        back();
+    }
+    if (OneMoreBack) back();
     ShowMessage("完成" + actionName);
 }
 
@@ -172,21 +211,36 @@ function IsMainForm() {
     return className("android.view.View").desc("搜索").depth(12).exists();
 }
 
+function IsSearchingForm() {
+    return className("android.widget.EditText").clickable(true).exists();
+}
+
 function CheckAndGoActivity(isBegining) {
     if (IsMainForm()) {
         ShowMessage("当前在主界面");
         ShowMessage("尝试进入618列车界面");
-        if(!TryGoActivityPage()) {
+        if (!GoActivityByButton() && !GoActivityBySearching()) {
             ShowMessage("进入主互动界面失败！");
             eixt();
         }
-        else{
+        else {
+            sleep(6000);
+        }
+    }
+    else if (IsSearchingForm()) {
+        ShowMessage("当前在搜索界面");
+        ShowMessage("尝试进入618列车界面");
+        if (!GoActivityBySearching()) {
+            ShowMessage("进入主互动界面失败！");
+            eixt();
+        }
+        else {
             sleep(6000);
         }
     }
     // 跳出领取祝福的处理
     var giftBtn = className("android.widget.Button").text("收下祝福").findOnce();
-    if(giftBtn){
+    if (giftBtn) {
         ShowMessage("领取祝福");
         giftBtn.click();
         sleep(1500);
@@ -195,34 +249,38 @@ function CheckAndGoActivity(isBegining) {
         descMatches("(.+)?我的列车(.+)?").exists() ||
         textMatches("(.+)?我的列车(.+)?").exists()) {
         if (!textMatches("关闭").exists()) {
-            ShowMessage("进入领瞄币页面");
+            ShowMessage("进入领喵币页面");
             sleep(1000);
             ClickLingmiaobi();
             sleep(1500);
         }
-        if (textMatches("关闭").exists() || textMatches("淘宝成就点").exists()) {
-            if (isBegining) {
-                ShowMessage("开始领取瞄币");
-            }
-        } else {
-            ShowMessage("无法检测到领瞄币窗口");
+        if (IsOnActivitySheet()) {
+            if (isBegining) ShowMessage("开始领取喵币");
+        }
+        else {
+            ShowMessage("无法检测到领喵币窗口");
             exit();
         }
     }
     else {
-        ShowMessage("无法找到领瞄币界面");
+        ShowMessage("无法找到领喵币界面");
         exit();
     }
 }
-
+function IsOnActivitySheet(){
+    return className("android.view.View").id("taskBottomSheet").exists() ||
+           textMatches("关闭").exists() ||
+           textMatches("淘宝成就点").exists();
+}
 function ClickLingmiaobi() {
-    var mbbtn = className("android.widget.Button").text("做任务，领喵币").findOnce();
-    if (mbbtn) {
-        mbbtn = mbbtn.parent();
-        var bnd = mbbtn.bounds();
-        click(bnd.centerX(), bnd.centerY());
+    if (!IsOnActivitySheet()) {
+        var mbbtn = className("android.widget.Button").text("做任务，领喵币").findOnce();
+        if (mbbtn) {
+            var mbbnd = mbbtn.bounds();
+            click(mbbnd.centerX(), mbbnd.centerY());
+        }
     }
-    else {
+    if (!IsOnActivitySheet()) {
         click(width * 0.9, height * 0.9);
     }
 }
@@ -238,10 +296,6 @@ function ClickMainPage() {
         click(width * 0.125, height - 50);
         sleep(1200);
     }
-}
-
-function TryGoActivityPage() {
-    return (GoActivityByButton() || GoActivityBySearching());
 }
 
 function GoActivityByButton() {
@@ -267,7 +321,7 @@ function TryClickGoAcivityBtn() {
     }
     else {
         return false;
-}
+    }
 }
 /**
  * @brief Go to activity page by searching method
@@ -276,27 +330,27 @@ function TryClickGoAcivityBtn() {
  */
 function GoActivityBySearching() {
     var searchBox = className("android.widget.FrameLayout").clickable(true).depth(13).findOnce();
-    if(searchBox){
+    if (searchBox) {
         searchBox.click();
         sleep(1000);
     }
 
-    if(IsOnSearching()){
+    if (IsOnSearching()) {
         searchContext = className("android.widget.EditText").clickable(true).findOnce();
         searchButton = className("android.widget.Button").desc("搜索").findOnce();
-        if(!searchContext){
+        if (!searchContext) {
             toast("搜索输入框不存在")
             return false;
         }
-        else if(!searchContext){
+        else if (!searchContext) {
             toast("搜索按钮不存在")
             return false;
         }
-        else{
+        else {
             searchContext.setText("618列车");
             searchButton.click();
         }
-    }else{
+    } else {
         toast("未能正常进入搜索界面")
         return false;
     }
@@ -306,6 +360,6 @@ function GoActivityBySearching() {
 /**
  * @brief check whether is on the second step of searching
  */
-function IsOnSearching(){
+function IsOnSearching() {
     return className("android.view.View").depth(8).desc("语音搜索").exists();
 }
